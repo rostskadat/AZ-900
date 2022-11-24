68721c7a209f1dd2d6b18b977eb2341c3c0f573a
Julien Masnada <rostskadat@gmail.com>
2022-11-24 13:58:29 +0100

    Adding new webapps

---
+++ Labs/Monitor Logs and Analytics in Azure Monitor/LabLogAnalytics/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LabLogAnalytics.Models;
+
+namespace LabLogAnalytics.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        _logger.LogWarning("Index accessed successfully");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Labs/Monitor Logs and Analytics in Azure Monitor/LabLogAnalytics/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LabLogAnalytics.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
